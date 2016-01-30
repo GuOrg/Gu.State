d4178970d59e815f21c7cf0c0e6b525eea9d85c1
Johan Larsson <johan_gavle1@hotmail.com>
2016-01-30 17:50:54 +0100

    Add ignore properties filter to DirtyTracker.

--- Gu.ChangeTracking/DirtyTracker.cs
+++ Gu.ChangeTracking/DirtyTracker.cs
@@ -4,10 +4,10 @@ namespace Gu.ChangeTracking
 
     public static class DirtyTracker
     {
-        public static DirtyTracker<T> Track<T>(T x, T y) 
+        public static DirtyTracker<T> Track<T>(T x, T y, params string[] ignoreProperties)
             where T : class, INotifyPropertyChanged
         {
-            return new DirtyTracker<T>(x, y);
+            return new DirtyTracker<T>(x, y, ignoreProperties);
         }
     }
 }
