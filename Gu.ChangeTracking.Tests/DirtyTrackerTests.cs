63c75d89a8d6abf436033f945c5e5540a30f84df
Johan Larsson <johan_gavle1@hotmail.com>
2016-01-30 03:46:08 +0100

    reminder

---
+++ Gu.ChangeTracking.Tests/DirtyTrackerTests.cs
@@ -0,0 +1,13 @@
+namespace Gu.ChangeTracking.Tests
+{
+    using NUnit.Framework;
+
+    public class DirtyTrackerTests
+    {
+        [Test]
+        public void Reminder()
+        {
+            Assert.Fail("Implement");
+        }
+    }
+}
