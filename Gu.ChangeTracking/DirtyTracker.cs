eb459b095e1137f41514e5d7a7ef217ef18eaf56
Johan Larsson <johan_gavle1@hotmail.com>
2016-01-31 10:16:08 +0100

    Move defaults to constants

--- Gu.ChangeTracking/DirtyTracker.cs
+++ Gu.ChangeTracking/DirtyTracker.cs
@@ -1,6 +1,7 @@
 namespace Gu.ChangeTracking
 {
     using System.ComponentModel;
+    using System.Reflection;
 
     public static class DirtyTracker
     {
@@ -10,10 +11,22 @@ namespace Gu.ChangeTracking
             return new DirtyTracker<T>(x, y, ignoreProperties);
         }
 
+        public static DirtyTracker<T> Track<T>(T x, T y, BindingFlags bindingFlags, params string[] ignoreProperties)
+            where T : class, INotifyPropertyChanged
+        {
+            return new DirtyTracker<T>(x, y, bindingFlags, ignoreProperties);
+        }
+
         public static void Verify<T>(params string[] ignoreProperties)
             where T : class, INotifyPropertyChanged
         {
             DirtyTracker<T>.Verify(ignoreProperties);
+        }
+
+        public static void Verify<T>(BindingFlags bindingFlags, params string[] ignoreProperties)
+            where T : class, INotifyPropertyChanged
+        {
+            DirtyTracker<T>.Verify(bindingFlags, ignoreProperties);
         }
     }
 }
--- Gu.ChangeTracking/PropertySynchronizer.cs
+++ Gu.ChangeTracking/PropertySynchronizer.cs
@@ -2,6 +2,7 @@ namespace Gu.ChangeTracking
 {
     using System;
     using System.ComponentModel;
+    using System.Reflection;
 
     public static class PropertySynchronizer
     {
@@ -11,9 +12,21 @@ namespace Gu.ChangeTracking
             return new PropertySynchronizer<T>(source, destination, excludedProperties);
         }
 
-        public static void VerifyCanSyncronize<T>( params string[] excludedProperties)
+        public static IDisposable Create<T>(T source, T destination, BindingFlags bindingFlags,
+            params string[] excludedProperties)
+            where T : class, INotifyPropertyChanged
+        {
+            return new PropertySynchronizer<T>(source, destination, bindingFlags, excludedProperties);
+        }
+
+        public static void VerifyCanSyncronize<T>(params string[] excludedProperties)
         {
             Copy.VerifyCanCopyPropertyValues<T>(excludedProperties);
+        }
+
+        public static void VerifyCanSyncronize<T>(BindingFlags bindingFlags, params string[] excludedProperties)
+        {
+            Copy.VerifyCanCopyPropertyValues<T>(bindingFlags, excludedProperties);
         }
     }
 }
