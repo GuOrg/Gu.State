aa88ff34404cbd3ff19a0e74ea4b605341098354
JohanLarsson <johan_gavle1@hotmail.com>
2016-02-19 09:49:37 +0100

    Prepare ItemCollection for reuse in DirtyTracker

---
+++ Gu.ChangeTracking/Internals/ItemCollection.cs
@@ -0,0 +1,85 @@
+﻿namespace Gu.ChangeTracking
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal sealed class ItemCollection<T> : IDisposable
+        where T : class, IDisposable
+    {
+        private readonly List<T> items = new List<T>();
+
+        public void Dispose()
+        {
+            this.Clear();
+        }
+
+        internal int Count => this.items.Count;
+
+        internal T this[int index]
+        {
+            get { return this.items[index]; }
+            set
+            {
+                if (index < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                }
+
+                this.SetItem(index, value);
+            }
+        }
+
+        internal void RemoveAt(int index)
+        {
+            this.TryGet(index)?.Dispose();
+            this.items.RemoveAt(index);
+        }
+
+        internal void Insert(int index, T item)
+        {
+            this.FillTo(index);
+            this.items.Insert(index, item);
+        }
+
+        internal void Move(int index, int toIndex)
+        {
+            var synchronizer = this.items[index];
+            this.items.RemoveAt(index);
+            this.Insert(toIndex, synchronizer);
+        }
+
+        internal void Clear()
+        {
+            for (int i = this.items.Count - 1; i >= 0; i--)
+            {
+                this.items[i]?.Dispose();
+                this.items.RemoveAt(i);
+            }
+        }
+
+        protected void SetItem(int index, T item)
+        {
+            this.FillTo(index);
+            this.TryGet(index)?.Dispose();
+            this.items[index] = item;
+        }
+
+        private void FillTo(int index)
+        {
+            while (this.items.Count <= index)
+            {
+                this.items.Add(null);
+            }
+        }
+
+        private T TryGet(int index)
+        {
+            if (index >= this.items.Count)
+            {
+                return null;
+            }
+
+            return this.items[index];
+        }
+    }
+}
