e583919be7f29c409996785c7de69e7b80dd5ff3
JohanLarsson <johan_gavle1@hotmail.com>
2016-02-22 14:50:56 +0100

    Add tests for struct and sealed constraint.

---
+++ Gu.ChangeTracking.Tests/Internals/Stubs/WithSelfFieldSealed.cs
@@ -0,0 +1,7 @@
+namespace Gu.ChangeTracking.Tests.Internals.Stubs
+{
+    public sealed class WithSelfFieldSealed
+    {
+        public readonly WithSelfFieldSealed Value;
+    }
+}
---
+++ Gu.ChangeTracking.Tests/Internals/Stubs/WithSelfPropSealed.cs
@@ -0,0 +1,7 @@
+namespace Gu.ChangeTracking.Tests.Internals.Stubs
+{
+    public sealed class WithSelfPropSealed
+    {
+        public WithSelfPropSealed Value { get; }
+    }
+}
