d1bda7a0212268513a599aa45fb1bce12fe8f2f3
JohanLarsson <johan_gavle1@hotmail.com>
2016-02-22 12:55:57 +0100

    Tweak ChangeTrackerTests

--- Gu.ChangeTracking.Tests/ChangeTracker/ChangeTrackerTests.Collection.cs
+++ Gu.ChangeTracking.Tests/ChangeTracker/ChangeTrackerTests.Collection.cs
@@ -2,7 +2,8 @@ namespace Gu.ChangeTracking.Tests
 {
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
-    using System.ComponentModel;
+    using System.Linq;
+
     using Gu.ChangeTracking.Tests.ChangeTrackerStubs;
     using NUnit.Framework;
 
@@ -10,169 +11,164 @@ namespace Gu.ChangeTracking.Tests
     {
         public class Collection
         {
-            private List<PropertyChangedEventArgs> changes;
-
-            [SetUp]
-            public void SetUp()
-            {
-                this.changes = new List<PropertyChangedEventArgs>();
-            }
-
             [Test]
             public void NotifiesOnAddInt()
             {
+                var changes = new List<string>();
                 var root = new Level();
                 using (var tracker = ChangeTracker.Track(root, ChangeTrackerSettings.Default))
                 {
-                    tracker.PropertyChanged += this.TrackerOnPropertyChanged;
+                    tracker.PropertyChanged += (_, e) => changes.Add(e.PropertyName);
+                    Assert.AreEqual(0, tracker.Changes);
+                    CollectionAssert.IsEmpty(changes);
 
                     root.Ints.Add(1);
                     Assert.AreEqual(1, tracker.Changes);
-                    Assert.AreEqual(1, this.changes.Count);
+                    CollectionAssert.AreEqual(new[] { "Changes" }, changes);
 
                     root.Ints = new ObservableCollection<int>();
                     Assert.AreEqual(2, tracker.Changes);
-                    Assert.AreEqual(2, this.changes.Count);
+                    CollectionAssert.AreEqual(new[] { "Changes", "Changes" }, changes);
 
                     root.Ints.Add(1);
                     Assert.AreEqual(3, tracker.Changes);
-                    Assert.AreEqual(3, this.changes.Count);
+                    CollectionAssert.AreEqual(new[] { "Changes", "Changes", "Changes" }, changes);
 
                     tracker.Dispose();
                     root.Ints.Add(2);
                     Assert.AreEqual(3, tracker.Changes);
-                    Assert.AreEqual(3, this.changes.Count);
-                    tracker.PropertyChanged -= this.TrackerOnPropertyChanged;
+                    CollectionAssert.AreEqual(new[] { "Changes", "Changes", "Changes" }, changes);
                 }
             }
 
             [Test]
             public void NotifiesOnAdd()
             {
+                var changes = new List<string>();
                 var root = new Level();
                 using (var tracker = ChangeTracker.Track(root, ChangeTrackerSettings.Default))
                 {
-                    tracker.PropertyChanged += this.TrackerOnPropertyChanged;
+                    tracker.PropertyChanged += (_, e) => changes.Add(e.PropertyName);
+                    Assert.AreEqual(0, tracker.Changes);
+                    CollectionAssert.IsEmpty(changes);
 
                     var level = new Level();
                     root.Levels.Add(level);
                     Assert.AreEqual(1, tracker.Changes);
-                    Assert.AreEqual(1, this.changes.Count);
+                    CollectionAssert.AreEqual(new[] { "Changes" }, changes);
 
                     level.Value++;
                     Assert.AreEqual(2, tracker.Changes);
-                    Assert.AreEqual(2, this.changes.Count);
+                    CollectionAssert.AreEqual(new[] { "Changes", "Changes" }, changes);
 
                     root.Levels.Add(level);
                     Assert.AreEqual(3, tracker.Changes);
-                    Assert.AreEqual(3, this.changes.Count);
+                    CollectionAssert.AreEqual(new[] { "Changes", "Changes", "Changes" }, changes);
 
                     root.Levels.Add(new Level());
                     Assert.AreEqual(4, tracker.Changes);
-                    Assert.AreEqual(4, this.changes.Count);
+                    CollectionAssert.AreEqual(new[] { "Changes", "Changes", "Changes", "Changes" }, changes);
 
                     root.Levels = new ObservableCollection<Level>();
                     Assert.AreEqual(5, tracker.Changes);
-                    Assert.AreEqual(5, this.changes.Count);
+                    CollectionAssert.AreEqual(Enumerable.Repeat("Changes", 5), changes);
 
                     tracker.Dispose();
 
                     level.Value++;
                     Assert.AreEqual(5, tracker.Changes);
-                    Assert.AreEqual(5, this.changes.Count);
+                    CollectionAssert.AreEqual(Enumerable.Repeat("Changes", 5), changes);
 
                     root.Levels.Add(new Level());
                     Assert.AreEqual(5, tracker.Changes);
-                    Assert.AreEqual(5, this.changes.Count);
-                    tracker.PropertyChanged -= this.TrackerOnPropertyChanged;
+                    CollectionAssert.AreEqual(Enumerable.Repeat("Changes", 5), changes);
                 }
             }
 
             [Test]
             public void NotifiesOnAddSpecialCollection()
             {
+                var changes = new List<string>();
                 var root = new SpecialCollection();
                 using (var tracker = ChangeTracker.Track(root, ChangeTrackerSettings.Default))
                 {
-                    tracker.PropertyChanged += this.TrackerOnPropertyChanged;
+                    tracker.PropertyChanged += (_, e) => changes.Add(e.PropertyName);
+                    Assert.AreEqual(0, tracker.Changes);
+                    CollectionAssert.IsEmpty(changes);
 
                     var level = new Level();
                     root.Add(level);
                     Assert.AreEqual(1, tracker.Changes);
-                    Assert.AreEqual(1, this.changes.Count);
+                    CollectionAssert.AreEqual(Enumerable.Repeat("Changes", 1), changes);
 
                     level.Value++;
                     Assert.AreEqual(2, tracker.Changes);
-                    Assert.AreEqual(2, this.changes.Count);
+                    CollectionAssert.AreEqual(Enumerable.Repeat("Changes", 2), changes);
 
                     root.Remove(level);
                     Assert.AreEqual(3, tracker.Changes);
-                    Assert.AreEqual(3, this.changes.Count);
+                    CollectionAssert.AreEqual(Enumerable.Repeat("Changes", 3), changes);
 
                     level.Value++;
                     Assert.AreEqual(3, tracker.Changes);
-                    Assert.AreEqual(3, this.changes.Count);
-                    tracker.PropertyChanged -= this.TrackerOnPropertyChanged;
+                    CollectionAssert.AreEqual(Enumerable.Repeat("Changes", 3), changes);
                 }
             }
 
             [Test]
             public void TracksAddedStopsOnRemoved()
             {
+                var changes = new List<string>();
                 var root = new Level();
                 using (var tracker = ChangeTracker.Track(root, ChangeTrackerSettings.Default))
                 {
-                    tracker.PropertyChanged += this.TrackerOnPropertyChanged;
+                    tracker.PropertyChanged += (_, e) => changes.Add(e.PropertyName);
+                    Assert.AreEqual(0, tracker.Changes);
+                    CollectionAssert.IsEmpty(changes);
 
                     var level = new Level();
                     root.Levels.Add(level);
                     Assert.AreEqual(1, tracker.Changes);
-                    Assert.AreEqual(1, this.changes.Count);
+                    CollectionAssert.AreEqual(Enumerable.Repeat("Changes", 1), changes);
 
                     level.Value++;
                     Assert.AreEqual(2, tracker.Changes);
-                    Assert.AreEqual(2, this.changes.Count);
+                    CollectionAssert.AreEqual(Enumerable.Repeat("Changes", 2), changes);
 
                     root.Levels.Clear();
                     Assert.AreEqual(3, tracker.Changes);
-                    Assert.AreEqual(3, this.changes.Count);
+                    CollectionAssert.AreEqual(Enumerable.Repeat("Changes", 3), changes);
 
                     level.Value++;
                     Assert.AreEqual(3, tracker.Changes);
-                    Assert.AreEqual(3, this.changes.Count);
-
-                    tracker.PropertyChanged -= this.TrackerOnPropertyChanged;
+                    CollectionAssert.AreEqual(Enumerable.Repeat("Changes", 3), changes);
                 }
             }
 
             [Test]
             public void NotifiesThreeLevels()
             {
+                var changes = new List<string>();
                 var root = new Level { Next = new Level { Next = new Level() } };
                 using (var tracker = ChangeTracker.Track(root, ChangeTrackerSettings.Default))
                 {
-                    tracker.PropertyChanged += this.TrackerOnPropertyChanged;
+                    tracker.PropertyChanged += (_, e) => changes.Add(e.PropertyName);
+                    Assert.AreEqual(0, tracker.Changes);
+                    CollectionAssert.IsEmpty(changes);
 
                     var level = new Level();
                     root.Levels.Add(level);
                     Assert.AreEqual(1, tracker.Changes);
-                    Assert.AreEqual(1, this.changes.Count);
+                    CollectionAssert.AreEqual(Enumerable.Repeat("Changes", 1), changes);
 
                     level.Next = new Level();
                     Assert.AreEqual(2, tracker.Changes);
-                    Assert.AreEqual(2, this.changes.Count);
+                    CollectionAssert.AreEqual(Enumerable.Repeat("Changes", 2), changes);
 
                     level.Next.Value++;
                     Assert.AreEqual(3, tracker.Changes);
-                    Assert.AreEqual(3, this.changes.Count);
-
-                    tracker.PropertyChanged -= this.TrackerOnPropertyChanged;
+                    CollectionAssert.AreEqual(Enumerable.Repeat("Changes", 3), changes);
                 }
-            }
-
-            private void TrackerOnPropertyChanged(object sender, PropertyChangedEventArgs e)
-            {
-                this.changes.Add(e);
             }
         }
     }
