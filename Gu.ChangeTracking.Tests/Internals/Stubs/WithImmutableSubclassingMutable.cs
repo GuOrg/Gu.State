a7d933aaabb5162923987ac18c5eb391067ac03a
Johan Larsson <johan_gavle1@hotmail.com>
2016-02-22 22:41:19 +0100

    Test some more edge cases for IsImmutable

---
+++ Gu.ChangeTracking.Tests/Internals/Stubs/WithImmutableSubclassingMutable.cs
@@ -0,0 +1,7 @@
+﻿namespace Gu.ChangeTracking.Tests.Internals.Stubs
+{
+    public class WithImmutableSubclassingMutable : WithMutableField
+    {
+        public readonly int ImmutableValue;
+    }
+}
