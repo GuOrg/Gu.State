7be54610c365a1dee3b888e305416061b085950e
JohanLarsson <johan_gavle1@hotmail.com>
2016-02-22 14:50:56 +0100

    Add tests for struct and sealed constraint.

---
+++ Gu.ChangeTracking.Tests/Internals/Stubs/WithGetReadOnlyPropertySealed.cs
@@ -0,0 +1,7 @@
+namespace Gu.ChangeTracking.Tests.Internals.Stubs
+{
+    public sealed class WithGetReadOnlyPropertySealed<T>
+    {
+        public T Value { get; }
+    }
+}
---
+++ Gu.ChangeTracking.Tests/Internals/Stubs/WithGetReadOnlyPropertyStruct.cs
@@ -0,0 +1,7 @@
+namespace Gu.ChangeTracking.Tests.Internals.Stubs
+{
+    public struct WithGetReadOnlyPropertyStruct<T>
+    {
+        public T Value { get; }
+    }
+}
