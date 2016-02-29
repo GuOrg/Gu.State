cb171f6fa4150d828878510692a36f86c7ba40c6
Johan Larsson <johan_gavle1@hotmail.com>
2016-02-29 21:58:15 +0100

    Tests for EqualBy.Collections

---
+++ Gu.ChangeTracking.Tests/EqualByTests/FieldValues/Collections.cs
@@ -0,0 +1,10 @@
+namespace Gu.ChangeTracking.Tests.EqualByTests.FieldValues
+{
+    public class Collections : CollectionTests
+    {
+        public override bool EqualByMethod<T>(T x, T y, ReferenceHandling referenceHandling)
+        {
+            return EqualBy.FieldValues(x, y, referenceHandling);
+        }
+    }
+}
--- Gu.ChangeTracking.Tests/EqualByTests/PropertyValues/Classes_.PropertyValues.cs
+++ Gu.ChangeTracking.Tests/EqualByTests/PropertyValues/Classes_.PropertyValues.cs
@@ -1,6 +1,5 @@
 ﻿namespace Gu.ChangeTracking.Tests.EqualByTests.PropertyValues
 {
-    using System.Collections.Generic;
     using System.Linq;
 
     using Gu.ChangeTracking.Tests.EqualByTests;
