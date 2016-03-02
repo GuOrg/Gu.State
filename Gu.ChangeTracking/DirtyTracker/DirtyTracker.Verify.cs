0aa4cdb33e8c16581573759f0b1ec91f2e072c66
JohanLarsson <johan_gavle1@hotmail.com>
2016-03-02 21:16:12 +0100

    docs for DirtyTracker.Verify

--- Gu.ChangeTracking/DirtyTracker/DirtyTracker.Verify.cs
+++ Gu.ChangeTracking/DirtyTracker/DirtyTracker.Verify.cs
@@ -29,12 +29,28 @@
             VerifyCanTrack<T>(settings);
         }
 
+        /// <summary>
+        /// Check if the properties of <typeparamref name="T"/> can be tracked.
+        /// This method will throw an exception if copy cannot be performed for <typeparamref name="T"/>
+        /// Read the exception message for detailed instructions about what is wrong.
+        /// Use this to fail fast or in unit tests.
+        /// </summary>
+        /// <typeparam name="T">The type to track</typeparam>
+        /// <param name="settings">Contains configuration for how tracking is performed</param>
         public static void VerifyCanTrack<T>(PropertiesSettings settings)
                         where T : class, INotifyPropertyChanged
         {
             VerifyCanTrack(typeof(T), settings);
         }
 
+        /// <summary>
+        /// Check if the properties of <paramref name="type"/> can be tracked.
+        /// This method will throw an exception if copy cannot be performed for <paramref name="type"/>
+        /// Read the exception message for detailed instructions about what is wrong.
+        /// Use this to fail fast or in unit tests.
+        /// </summary>
+        /// <param name="type">The type to track</param>
+        /// <param name="settings">Contains configuration for how tracking is performed</param>
         public static void VerifyCanTrack(Type type, PropertiesSettings settings)
         {
             ChangeTracker.VerifyCanTrack(type, settings);
