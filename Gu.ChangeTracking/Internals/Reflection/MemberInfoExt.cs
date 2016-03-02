76b4e17dd7f94a4ddc4f96d2c183a6af85bd6760
JohanLarsson <johan_gavle1@hotmail.com>
2016-03-02 12:55:14 +0100

    Extracted some shared exception builder stuff.

---
+++ Gu.ChangeTracking/Internals/Reflection/MemberInfoExt.cs
@@ -0,0 +1,26 @@
+namespace Gu.ChangeTracking
+{
+    using System;
+    using System.Reflection;
+
+    internal static class MemberInfoExt
+    {
+        internal static Type GetMemberType(this MemberInfo memberInfo)
+        {
+            var fieldInfo = memberInfo as FieldInfo;
+            if (fieldInfo != null)
+            {
+                return fieldInfo.FieldType;
+            }
+
+            var propertyInfo = memberInfo as PropertyInfo;
+            if (propertyInfo != null)
+            {
+                return propertyInfo.PropertyType;
+            }
+
+            Throw.ThrowThereIsABugInTheLibraryExpectedParameterOfTypes<FieldInfo, PropertyInfo>(nameof(memberInfo));
+            throw new InvalidOperationException("Never getting here");
+        }
+    }
+}
