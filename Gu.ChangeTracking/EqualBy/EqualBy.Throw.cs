801264297e020c5c067b9effa6ecf938015a85ca
JohanLarsson <johan_gavle1@hotmail.com>
2016-03-03 18:53:23 +0100

    Remove unused stuff.

--- Gu.ChangeTracking/EqualBy/EqualBy.Throw.cs
+++ Gu.ChangeTracking/EqualBy/EqualBy.Throw.cs
@@ -6,17 +6,6 @@
 
     public static partial class EqualBy
     {
-        private static StringBuilder AppendSuggestEqualBySettings<T>(this StringBuilder messageBuilder, Type type, MemberInfo member)
-            where T : class, IMemberSettings
-        {
-            return messageBuilder.CreateIfNull()
-                                 .AppendLine($"* Use {typeof(T).Name} and specify how comparing is performed:")
-                                 .AppendLine($"  - {typeof(ReferenceHandling).Name}.{nameof(ReferenceHandling.Structural)} means that a deep equals is performed.")
-                                 .AppendLine($"  - {typeof(ReferenceHandling).Name}.{nameof(ReferenceHandling.References)} means that reference equality is used.")
-                                 .AppendExcludeType(type)
-                                 .AppendExcludeMember(type, member);
-        }
-
         private static StringBuilder AppendEqualByFailed<T>(this StringBuilder errorBuilder)
             where T : class, IMemberSettings
         {
@@ -70,45 +59,6 @@
 
             var message = errorBuilder.ToString();
             throw new NotSupportedException(message);
-        }
-
-        private static class Throw
-        {
-            internal static void CannotCompareMember<T>(Type sourceType, MemberInfo member)
-                where T : class, IMemberSettings
-            {
-                var errorBuilder = new StringBuilder();
-                Type memberType = null;
-                var propertyInfo = member as PropertyInfo;
-                if (propertyInfo != null)
-                {
-                    errorBuilder.AppendEqualByFailed<PropertiesSettings>();
-                    errorBuilder.AppendLine($"The property {sourceType.PrettyName()}.{propertyInfo.Name} is not supported.");
-                    errorBuilder.AppendLine($"The property is of type {propertyInfo.PropertyType.PrettyName()}.");
-                    memberType = propertyInfo.PropertyType;
-                }
-                else
-                {
-                    var fieldInfo = member as FieldInfo;
-                    if (fieldInfo != null)
-                    {
-                        errorBuilder.AppendEqualByFailed<FieldsSettings>();
-                        errorBuilder.AppendLine($"The field {sourceType.PrettyName()}.{fieldInfo.Name} is not supported.");
-                        errorBuilder.AppendLine($"The field is of type {fieldInfo.FieldType.PrettyName()}.");
-                        memberType = fieldInfo.FieldType;
-                    }
-                    else
-                    {
-                        throw Gu.ChangeTracking.Throw.ExpectedParameterOfTypes<PropertyInfo, FieldInfo>(nameof(member));
-                    }
-                }
-
-                errorBuilder.AppendSolveTheProblemBy()
-                            .AppendSuggestImplementIEquatable(memberType)
-                            .AppendSuggestEqualBySettings<T>(sourceType, member);
-                var message = errorBuilder.ToString();
-                throw new NotSupportedException(message);
-            }
         }
     }
 }
