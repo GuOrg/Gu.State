06d4f2bddbbb2061989126d63ca9cdecfa245de2
JohanLarsson <johan_gavle1@hotmail.com>
2016-03-04 11:46:15 +0100

    Failing test for detecting reference loops in EqualBy.Verify.

--- Gu.ChangeTracking.Tests/EqualByTests/VerifyTests.cs
+++ Gu.ChangeTracking.Tests/EqualByTests/VerifyTests.cs
@@ -9,6 +9,36 @@ namespace Gu.ChangeTracking.Tests.EqualByTests
         public abstract void VerifyMethod<T>(ReferenceHandling referenceHandling = ReferenceHandling.Throw, string excludedMembers = null, Type excludedType = null);
 
         [Test]
+        public void DetectesReferenceLoop()
+        {
+            var expected = this.GetType() == typeof(FieldValues.Verify)
+                   ? "EqualBy.FieldValues(x, y) failed.\r\n" +
+                     "The field WithComplexProperty.<ComplexType>k__BackingField of type ComplexType is not supported.\r\n" +
+                     "Solve the problem by any of:\r\n" +
+                     "* Implement IEquatable<ComplexType> for ComplexType or use a type that does.\r\n" +
+                     "* Use FieldsSettings and specify how comparing is performed:\r\n" +
+                     "  - ReferenceHandling.Structural means that a deep equals is performed.\r\n" +
+                     "  - ReferenceHandling.References means that reference equality is used.\r\n" +
+                     "  - Exclude the type WithComplexProperty.\r\n" +
+                     "  - Exclude the field WithComplexProperty.<ComplexType>k__BackingField.\r\n"
+
+                   : "EqualBy.PropertyValues(x, y) failed.\r\n" +
+                     "The property WithComplexProperty.ComplexType of type ComplexType is not supported.\r\n" +
+                     "Solve the problem by any of:\r\n" +
+                     "* Implement IEquatable<ComplexType> for ComplexType or use a type that does.\r\n" +
+                     "* Use PropertiesSettings and specify how comparing is performed:\r\n" +
+                     "  - ReferenceHandling.Structural means that a deep equals is performed.\r\n" +
+                     "  - ReferenceHandling.References means that reference equality is used.\r\n" +
+                     "  - Exclude the type WithComplexProperty.\r\n" +
+                     "  - Exclude the property WithComplexProperty.ComplexType.\r\n";
+            var exception = Assert.Throws<NotSupportedException>(() => this.VerifyMethod<EqualByTypes.Parent>(ReferenceHandling.Structural));
+            Assert.AreEqual(expected, exception.Message);
+
+            Assert.DoesNotThrow(() => this.VerifyMethod<EqualByTypes.Parent>(ReferenceHandling.StructuralWithReferenceLoops));
+            Assert.DoesNotThrow(() => this.VerifyMethod<EqualByTypes.Parent>(ReferenceHandling.References));
+        }
+
+        [Test]
         public void ComplexValueThrowsWithoutReferenceHandling()
         {
             var expected = this.GetType() == typeof(FieldValues.Verify)
