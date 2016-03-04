a41d8ad370101d31733c9b40f37dc03ed88f2bc3
Johan Larsson <johan_gavle1@hotmail.com>
2016-03-04 08:19:27 +0100

    Amend, implement IDisposable

--- Gu.ChangeTracking/Internals/Collections/TwoItemsTrackerReferenceCollection.cs
+++ Gu.ChangeTracking/Internals/Collections/TwoItemsTrackerReferenceCollection.cs
@@ -14,6 +14,7 @@
         {
             foreach (var trackerReference in this.items)
             {
+                trackerReference.RefCount = 0;
                 trackerReference.Tracker.Dispose();
             }
 
