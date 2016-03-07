2e4497eec7fc998143ebcd56bca2fe889facad48
JohanLarsson <johan_gavle1@hotmail.com>
2016-03-07 13:47:45 +0100

    Remove unused.

--- Gu.ChangeTracking/ExceptionHelpers/StringBuilderExt.cs
+++ Gu.ChangeTracking/ExceptionHelpers/StringBuilderExt.cs
@@ -1,25 +1,12 @@
 ﻿namespace Gu.ChangeTracking
 {
-    using System;
-    using System.Collections;
-    using System.Collections.Generic;
-    using System.Collections.Specialized;
-    using System.Linq;
-    using System.Reflection;
     using System.Text;
 
     internal static partial class StringBuilderExt
     {
-
-
         internal static StringBuilder AppendSolveTheProblemBy(this StringBuilder errorBuilder)
         {
             return errorBuilder.AppendLine("Solve the problem by any of:");
-        }
-
-        internal static StringBuilder CreateIfNull(this StringBuilder errorBuilder)
-        {
-            return errorBuilder ?? new StringBuilder();
         }
     }
 }
