e5143302f6af866258459e4f59ff057a5d809830
JohanLarsson <johan_gavle1@hotmail.com>
2016-03-09 11:04:32 +0100

    DisposingMap & ImmutableList

---
+++ Gu.State.Tests/Internals/Collections/DisposingMapTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Gu.State.Tests.Internals.Collections
+{
+    using System;
+    using Moq;
+    using NUnit.Framework;
+
+    public class DisposingMapTests
+    {
+        [Test]
+        public void SetDisposesOld()
+        {
+            var disposableMock1 = new Mock<IDisposable>();
+            var collection = new DisposingMap<int, IDisposable>();
+            collection.SetValue(1, disposableMock1.Object);
+            disposableMock1.Verify(x => x.Dispose(), Times.Never);
+            var disposableMock2 = new Mock<IDisposable>();
+            collection.SetValue(1, disposableMock2.Object);
+
+            disposableMock1.Verify(x => x.Dispose(), Times.Once);
+            disposableMock2.Verify(x => x.Dispose(), Times.Never);
+        }
+
+        [Test]
+        public void SetNullDisposesOld()
+        {
+            var disposableMock1 = new Mock<IDisposable>();
+            var collection = new DisposingMap<int, IDisposable>();
+            collection.SetValue(1, disposableMock1.Object);
+
+            disposableMock1.Verify(x => x.Dispose(), Times.Never);
+            collection.SetValue(1, null);
+
+            disposableMock1.Verify(x => x.Dispose(), Times.Once);
+        }
+
+        [Test]
+        public void Dispose()
+        {
+            var disposableMock1 = new Mock<IDisposable>();
+            var collection = new DisposingMap<int, IDisposable>();
+            collection.SetValue(1, disposableMock1.Object);
+
+            collection.Dispose();
+            disposableMock1.Verify(x => x.Dispose(), Times.Once);
+        }
+    }
+}
---
+++ Gu.State/Internals/Collections/DisposingMap{TKey, TValue}.cs
@@ -0,0 +1,52 @@
+﻿namespace Gu.State
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Linq;
+
+    internal sealed class DisposingMap<TKey, TValue> : IDisposable
+        where TValue : IDisposable
+    {
+        private readonly ConcurrentDictionary<TKey, TValue> items = new ConcurrentDictionary<TKey, TValue>();
+        private bool disposed;
+
+        public void Dispose()
+        {
+            if (this.disposed)
+            {
+                return;
+            }
+
+            this.disposed = true;
+            while (this.items.Count > 0)
+            {
+                TValue item;
+                if (this.items.TryRemove(this.items.Keys.First(), out item))
+                {
+                    item?.Dispose();
+                }
+            }
+        }
+
+        internal void SetValue(TKey key, TValue value)
+        {
+            this.VerifyDisposed();
+            this.items.AddOrUpdate(
+                key,
+                k => value,
+                (k, v) =>
+                {
+                    v?.Dispose();
+                    return value;
+                });
+        }
+
+        private void VerifyDisposed()
+        {
+            if (this.disposed)
+            {
+                throw new ObjectDisposedException(this.GetType().FullName);
+            }
+        }
+    }
+}
