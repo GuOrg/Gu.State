0e1280a9e90e1c2c4e29ac9899485aa97495bc73
Johan Larsson <johan_gavle1@hotmail.com>
2016-04-06 12:44:11 +0200

    EnumerableEqualByComparer

---
+++ Gu.State/EqualBy/Comparers/EnumerableEqualByComparer{T}.cs
@@ -0,0 +1,66 @@
+﻿namespace Gu.State
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class EnumerableEqualByComparer<T> : EqualByComparer
+    {
+        public static readonly EnumerableEqualByComparer<T> Default = new EnumerableEqualByComparer<T>();
+
+        private EnumerableEqualByComparer()
+        {
+        }
+
+        public override bool Equals<TSetting>(
+            object x,
+            object y,
+            Func<object, object, TSetting, ReferencePairCollection, bool> compareItem,
+            TSetting settings,
+            ReferencePairCollection referencePairs)
+        {
+            bool result;
+            if (TryGetEitherNullEquals(x, y, out result))
+            {
+                return result;
+            }
+
+            var isEquatable = settings.IsEquatable(typeof(T));
+            if (settings.ReferenceHandling == ReferenceHandling.References)
+            {
+                return isEquatable
+                           ? ItemsEquals((IEnumerable<T>)x, (IEnumerable<T>)y, EqualityComparer<T>.Default.Equals)
+                           : ItemsEquals((IEnumerable<T>)x, (IEnumerable<T>)y, (xi, yi) => ReferenceEquals(xi, yi));
+            }
+
+            return isEquatable
+                       ? ItemsEquals((IEnumerable<T>)x, (IEnumerable<T>)y, EqualityComparer<T>.Default.Equals)
+                       : ItemsEquals((IEnumerable<T>)x, (IEnumerable<T>)y, (xi, yi) => compareItem(xi, yi, settings, referencePairs));
+        }
+
+        private static bool ItemsEquals(IEnumerable<T> x, IEnumerable<T> y, Func<T, T, bool> compare)
+        {
+            using (var xe = x.GetEnumerator())
+            {
+                using (var ye = y.GetEnumerator())
+                {
+                    while (true)
+                    {
+                        var xn = xe.MoveNext();
+                        var yn = ye.MoveNext();
+                        if (xn && yn)
+                        {
+                            if (!compare(xe.Current, ye.Current))
+                            {
+                                return false;
+                            }
+
+                            continue;
+                        }
+
+                        return !xn && !yn;
+                    }
+                }
+            }
+        }
+    }
+}
