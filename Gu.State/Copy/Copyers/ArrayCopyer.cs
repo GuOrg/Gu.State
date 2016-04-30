8ad383fea27cc7de7b94a4ea5ae05e1ef63187bf
Johan Larsson <johan_gavle1@hotmail.com>
2016-04-30 12:12:18 +0200

    ArrayCopyer

---
+++ Gu.State/Copy/Copyers/ArrayCopyer.cs
@@ -0,0 +1,207 @@
+﻿namespace Gu.State
+{
+    using System;
+    using System.Globalization;
+    using System.Reflection;
+
+    public class ArrayCopyer : ICopyer
+    {
+        public static readonly ArrayCopyer Default = new ArrayCopyer();
+
+        private ArrayCopyer()
+        {
+        }
+
+        public static bool TryGetOrCreate(object x, object y, out ICopyer comparer)
+        {
+            if (x is Array && y is Array)
+            {
+                comparer = Default;
+                return true;
+            }
+
+            comparer = null;
+            return false;
+        }
+
+        public void CopyItems<TSettings>(
+            object source,
+            object target,
+            Action<object, object, TSettings, ReferencePairCollection> syncItem,
+            TSettings settings,
+            ReferencePairCollection referencePairs)
+            where TSettings : class, IMemberSettings
+        {
+            Copy((Array)source, (Array)target, syncItem, settings, referencePairs);
+        }
+
+        private static void Copy<TSettings>(
+            Array sourceArray,
+            Array targetArray,
+            Action<object, object, TSettings, ReferencePairCollection> syncItem,
+            TSettings settings,
+            ReferencePairCollection referencePairs)
+            where TSettings : class, IMemberSettings
+        {
+            if (!Is.SameSize(sourceArray, targetArray))
+            {
+                throw State.Copy.Throw.CannotCopyFixesSizeCollections(sourceArray, targetArray, settings);
+            }
+
+            var itemType = sourceArray.GetType().GetItemType();
+            var rank = sourceArray.Rank;
+            if (rank < 4)
+            {
+                string methodName;
+                switch (rank)
+                {
+                    case 1:
+                        methodName = nameof(Copy1DItems);
+                        break;
+                    case 2:
+                        methodName = nameof(Copy2DItems);
+                        break;
+                    case 3:
+                        methodName = nameof(Copy3DItems);
+                        break;
+                    default:
+                        throw Throw.ShouldNeverGetHereException("Expected rank {1, 2, 3} was : " + rank);
+                }
+
+                var copyMethod = typeof(ArrayCopyer).GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Static)
+                                                    .MakeGenericMethod(itemType, typeof(TSettings));
+                copyMethod.Invoke(null, new object[] { sourceArray, targetArray, syncItem, settings, referencePairs });
+            }
+            else
+            {
+                CopyAnyDimension(sourceArray, targetArray, syncItem, settings, referencePairs);
+            }
+        }
+
+        private static void Copy1DItems<T, TSettings>(
+            T[] sourceArray,
+            T[] targetArray,
+            Action<object, object, TSettings, ReferencePairCollection> syncItem,
+            TSettings settings,
+            ReferencePairCollection referencePairs)
+            where TSettings : class, IMemberSettings
+        {
+            var isImmutable = settings.IsImmutable(
+                sourceArray.GetType()
+                           .GetItemType());
+            for (var i = 0; i < sourceArray.Length; i++)
+            {
+                var sv = sourceArray[i];
+                var tv = targetArray[i];
+                var copyItem = CopyItem(sv, tv, syncItem, settings, referencePairs, isImmutable);
+                targetArray[i] = copyItem;
+            }
+        }
+
+        private static void Copy2DItems<T, TSettings>(
+            T[,] sourceArray,
+            T[,] targetArray,
+            Action<object, object, TSettings, ReferencePairCollection> syncItem,
+            TSettings settings,
+            ReferencePairCollection referencePairs)
+            where TSettings : class, IMemberSettings
+        {
+            var isImmutable = settings.IsImmutable(
+                sourceArray.GetType()
+                           .GetItemType());
+            for (var i = sourceArray.GetLowerBound(0); i <= sourceArray.GetUpperBound(0); i++)
+            {
+                for (var j = sourceArray.GetLowerBound(1); j <= sourceArray.GetUpperBound(1); j++)
+                {
+                    var sv = sourceArray[i, j];
+                    var tv = targetArray[i, j];
+                    var copyItem = CopyItem(sv, tv, syncItem, settings, referencePairs, isImmutable);
+                    targetArray[i, j] = copyItem;
+                }
+            }
+        }
+
+        private static void Copy3DItems<T, TSettings>(
+            T[,,] sourceArray,
+            T[,,] targetArray,
+            Action<object, object, TSettings, ReferencePairCollection> syncItem,
+            TSettings settings,
+            ReferencePairCollection referencePairs)
+            where TSettings : class, IMemberSettings
+        {
+            var isImmutable = settings.IsImmutable(
+                sourceArray.GetType()
+                           .GetItemType());
+            for (var i = sourceArray.GetLowerBound(0); i <= sourceArray.GetUpperBound(0); i++)
+            {
+                for (var j = sourceArray.GetLowerBound(1); j <= sourceArray.GetUpperBound(1); j++)
+                {
+                    for (var k = sourceArray.GetLowerBound(2); k <= sourceArray.GetUpperBound(2); k++)
+                    {
+                        var sv = sourceArray[i, j, k];
+                        var tv = targetArray[i, j, k];
+                        var copyItem = CopyItem(sv, tv, syncItem, settings, referencePairs, isImmutable);
+                        targetArray[i, j, k] = copyItem;
+                    }
+                }
+            }
+        }
+
+        private static void CopyAnyDimension<TSettings>(
+            Array sourceArray,
+            Array targetArray,
+            Action<object, object, TSettings, ReferencePairCollection> syncItem,
+            TSettings settings,
+            ReferencePairCollection referencePairs)
+            where TSettings : class, IMemberSettings
+        {
+            var isImmutable = settings.IsImmutable(
+                sourceArray.GetType()
+                           .GetItemType());
+            foreach (var index in sourceArray.Indices())
+            {
+                var sv = sourceArray.GetValue(index);
+                var tv = targetArray.GetValue(index);
+                var copyItem = CopyItem(sv, tv, syncItem, settings, referencePairs, isImmutable);
+                targetArray.SetValue(copyItem, index);
+            }
+        }
+
+        private static T CopyItem<T, TSettings>(
+            T sourceItem,
+            T targetItem,
+            Action<object, object, TSettings, ReferencePairCollection> syncItem,
+            TSettings settings,
+            ReferencePairCollection referencePairs,
+            bool isImmutable)
+            where TSettings : class, IMemberSettings
+        {
+            if (sourceItem == null || settings.ReferenceHandling == ReferenceHandling.References || isImmutable)
+            {
+                return sourceItem;
+            }
+
+            switch (settings.ReferenceHandling)
+            {
+                case ReferenceHandling.References:
+                    return sourceItem;
+                case ReferenceHandling.Structural:
+                case ReferenceHandling.StructuralWithReferenceLoops:
+                    if (targetItem == null)
+                    {
+                        targetItem = (T)State.Copy.CreateInstance(sourceItem, null, settings);
+                    }
+
+                    syncItem(sourceItem, targetItem, settings, referencePairs);
+                    return targetItem;
+                case ReferenceHandling.Throw:
+                    throw State.Throw.ShouldNeverGetHereException();
+                default:
+                    throw new ArgumentOutOfRangeException(
+                        nameof(settings.ReferenceHandling),
+                        settings.ReferenceHandling,
+                        null);
+            }
+        }
+    }
+}
---
+++ Gu.State/Copy/Copyers/ICopyer.cs
@@ -0,0 +1,15 @@
+﻿namespace Gu.State
+{
+    using System;
+
+    public interface ICopyer
+    {
+        void CopyItems<TSettings>(
+            object source,
+            object target,
+            Action<object, object, TSettings, ReferencePairCollection> syncItem,
+            TSettings settings,
+            ReferencePairCollection referencePairs)
+            where TSettings : class, IMemberSettings;
+    }
+}
