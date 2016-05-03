c49715b03a7a722baa6b76d43a1bcae7e7362ac9
Johan Larsson <johan_gavle1@hotmail.com>
2016-05-03 10:33:14 +0200

    Use TryGetValueEquals in TryGetValueDiff

--- Gu.State/DiffBy/DiffBy.Shared.cs
+++ Gu.State/DiffBy/DiffBy.Shared.cs
@@ -20,29 +20,13 @@
         private static bool TryGetValueDiff<TSettings>(object x, object y, TSettings settings, out ValueDiff diff)
             where TSettings : IMemberSettings
         {
-            if (ReferenceEquals(x, y))
+            bool result;
+            if (EqualBy.TryGetValueEquals(x, y, settings, out result))
             {
-                diff = null;
-                return true;
-            }
-
-            if (x == null || y == null)
-            {
-                diff = new ValueDiff(x, y);
-                return true;
-            }
-
-            if (x.GetType() != y.GetType())
-            {
-                diff = new ValueDiff(x, y);
-                return true;
-            }
-
-            if (settings.IsEquatable(x.GetType()))
-            {
-                diff = Equals(x, y)
+                diff = result
                            ? null
                            : new ValueDiff(x, y);
+
                 return true;
             }
 
