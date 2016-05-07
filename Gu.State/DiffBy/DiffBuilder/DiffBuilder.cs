66164e15e2ed91336bcae36fb7ac32941c3ad15d
Johan Larsson <johan_gavle1@hotmail.com>
2016-05-07 12:23:32 +0200

    Add subbuilders.

--- Gu.State/DiffBy/DiffBuilder/DiffBuilder.cs
+++ Gu.State/DiffBy/DiffBuilder/DiffBuilder.cs
@@ -9,31 +9,36 @@
     internal sealed class DiffBuilder : IDisposable
     {
         private readonly List<SubDiff> diffs = new List<SubDiff>();
-        private readonly List<Disposer<DiffBuilder>> disposers = new List<Disposer<DiffBuilder>>();
+        private readonly IBorrowed<Dictionary<object, IRefCounted<DiffBuilder>>> subBuilders;
+        private readonly object gate = new object();
         private readonly ValueDiff valueDiff;
         private bool disposed;
 
         private DiffBuilder(object x, object y)
         {
             this.valueDiff = new ValueDiff(x, y, this.diffs);
+            this.subBuilders = DictionaryPool<object, IRefCounted<DiffBuilder>>.Borrow();
         }
 
         private bool IsEmpty => this.diffs.All(d => d.IsEmpty);
 
         public void Dispose()
         {
-            if (this.disposed)
+            lock (this.gate)
             {
-                return;
-            }
+                if (this.disposed)
+                {
+                    return;
+                }
 
-            this.disposed = true;
-            foreach (var disposer in this.disposers)
-            {
-                disposer.Dispose();
-            }
+                this.disposed = true;
+                foreach (var disposer in this.subBuilders.Value.Values)
+                {
+                    disposer.Dispose();
+                }
 
-            this.disposers.Clear();
+                this.subBuilders.Dispose();
+            }
         }
 
         internal static IRefCounted<DiffBuilder> Create(object x, object y, IMemberSettings settings)
@@ -63,12 +68,14 @@
         {
             Debug.Assert(!this.disposed, "this.disposed");
             this.diffs.Add(MemberDiff.Create(member, builder.valueDiff));
+            this.AddSubBuilder(member, builder);
         }
 
         internal void AddLazy(object index, DiffBuilder builder)
         {
             Debug.Assert(!this.disposed, "this.disposed");
             this.diffs.Add(new IndexDiff(index, builder.valueDiff));
+            this.AddSubBuilder(index, builder);
         }
 
         internal ValueDiff CreateValueDiff()
@@ -78,10 +85,21 @@
             return this.IsEmpty ? null : this.valueDiff;
         }
 
+        private void AddSubBuilder(object key, DiffBuilder builder)
+        {
+            IRefCounted<DiffBuilder> refCounted;
+            if (!builder.TryRefCount(out refCounted))
+            {
+                throw Throw.ShouldNeverGetHereException("AddLazy failed, try refcount failed");
+            }
+
+            this.subBuilders.Value[key] = refCounted;
+        }
+
         private void PurgeEmpty()
         {
-            throw new NotImplementedException("message");
-            
+            //throw new NotImplementedException("message");
+
             //foreach (var builder in this.builderCache.Values)
             //{
             //    if (builder.IsEmpty)
