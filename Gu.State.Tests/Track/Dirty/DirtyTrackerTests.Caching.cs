5158f131a1f655646f236d3ce233dbf081f2282e
Johan Larsson <johan_gavle1@hotmail.com>
2016-05-09 15:20:04 +0200

    More tests.

--- Gu.State.Tests/Track/Dirty/DirtyTrackerTests.Caching.cs
+++ Gu.State.Tests/Track/Dirty/DirtyTrackerTests.Caching.cs
@@ -20,6 +20,26 @@ namespace Gu.State.Tests
                 var t3 = Track.IsDirty(x, y);
                 Assert.AreNotSame(t1, t3);
             }
+
+            [Test]
+            public void ReturnsDifferentForDifferentPairs()
+            {
+                var x = new DirtyTrackerTypes.SimpleDirtyTrackClass { Value1 = 1, Value2 = 2 };
+                var y = new DirtyTrackerTypes.SimpleDirtyTrackClass { Value1 = 1, Value2 = 2 };
+                var t1 = Track.IsDirty(x, y);
+                var t2 = Track.IsDirty(y, x);
+                Assert.AreNotSame(t1, t2);
+            }
+
+            [Test]
+            public void ReturnsDifferentForDifferentSettings()
+            {
+                var x = new DirtyTrackerTypes.SimpleDirtyTrackClass { Value1 = 1, Value2 = 2 };
+                var y = new DirtyTrackerTypes.SimpleDirtyTrackClass { Value1 = 1, Value2 = 2 };
+                var t1 = Track.IsDirty(x, y, PropertiesSettings.GetOrCreate(referenceHandling: ReferenceHandling.Structural));
+                var t2 = Track.IsDirty(x, y, PropertiesSettings.GetOrCreate(referenceHandling: ReferenceHandling.References));
+                Assert.AreNotSame(t1, t2);
+            }
         }
     }
 }
