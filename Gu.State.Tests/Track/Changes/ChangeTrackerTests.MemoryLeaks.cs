2070c63b33f54cbd82910460ce3f9787fca42f07
Johan Larsson <johan_gavle1@hotmail.com>
2016-05-10 08:43:52 +0200

    Memory leak tests.

--- Gu.State.Tests/Track/Changes/ChangeTrackerTests.MemoryLeaks.cs
+++ Gu.State.Tests/Track/Changes/ChangeTrackerTests.MemoryLeaks.cs
@@ -21,22 +21,28 @@
             public void ComplexType()
             {
                 var item = new ComplexType();
+                WeakReference weakReference = new WeakReference(item);
                 var tracker = Track.Changes(item, ReferenceHandling.Structural);
-                var weakReference = new WeakReference(item);
-
+                tracker.Dispose();
                 item = null;
                 GC.Collect();
-                Assert.IsTrue(weakReference.IsAlive);
-
-                tracker.Dispose();
-                GC.Collect();
                 Assert.IsFalse(weakReference.IsAlive);
+                Assert.NotNull(tracker); // touching it here so it is not optimized away.
             }
 
             [Test]
             public void DoesNotLeakLevel()
             {
-                Assert.Fail("Track levels and remove.Level. Then check that it is not kept alive.");
+                var item = new With<ComplexType> { Value = new ComplexType() };
+                using (var tracker = Track.Changes(item, ReferenceHandling.Structural))
+                {
+                    var weakReference = new WeakReference(item.Value);
+                    item.Value = null;
+                    GC.Collect();
+                    Assert.IsFalse(weakReference.IsAlive);
+                }
+
+                Assert.NotNull(item); // touching it here so it is not optimized away.
             }
         }
     }
