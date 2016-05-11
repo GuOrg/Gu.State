98cf671603f6b15bef129b3f4ec779fd0870d5f1
Johan Larsson <johan_gavle1@hotmail.com>
2016-05-11 14:04:04 +0200

    Add asserts.

--- Gu.State.Tests/Track/Dirty/DirtyTrackerNodeTests.Caching.cs
+++ Gu.State.Tests/Track/Dirty/DirtyTrackerNodeTests.Caching.cs
@@ -10,19 +10,22 @@ namespace Gu.State.Tests
         public class Caching
         {
             [Test]
-            public void ReturnsSameForSameWhileAlive()
+            public void ReturnsSameWhileAlive()
             {
-                var x = new WithSimpleProperties { Value1 = 1, Value2 = 2 };
-                var y = new WithSimpleProperties { Value1 = 1, Value2 = 2 };
+                var x = new WithSimpleProperties();
+                var y = new WithSimpleProperties();
                 var settings = PropertiesSettings.GetOrCreate(ReferenceHandling.Structural);
                 var t1 = DirtyTrackerNode.GetOrCreate(x, y, settings, true);
                 var t2 = DirtyTrackerNode.GetOrCreate(x, y, settings, true);
                 Assert.AreSame(t1, t2);
                 t1.Dispose();
-                t2.Dispose();
-
                 var t3 = DirtyTrackerNode.GetOrCreate(x, y, settings, true);
-                Assert.AreNotSame(t1, t3);
+                Assert.AreSame(t1, t3);
+                t2.Dispose();
+                t3.Dispose();
+
+                var t4 = DirtyTrackerNode.GetOrCreate(x, y, settings, true);
+                Assert.AreNotSame(t1, t4);
             }
 
             [Test]
