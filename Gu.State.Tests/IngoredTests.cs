7f913d7a474ad11a3fec7dd52b06ecb7a196c220
Johan Larsson <johan_gavle1@hotmail.com>
2016-05-11 16:08:00 +0200

    IgnoredTests

---
+++ Gu.State.Tests/IngoredTests.cs
@@ -0,0 +1,7 @@
+﻿namespace Gu.State.Tests
+{
+    public static class IngoredTests
+    {
+        public const string MemoryLeak = "Think this test is broken.DotMemoryUnit says 'this is fine'";
+    }
+}
