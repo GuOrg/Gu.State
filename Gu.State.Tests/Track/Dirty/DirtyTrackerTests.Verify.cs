b4f710ef6ddf2d15d217dbc61888ba56b28fa112
Johan Larsson <johan_gavle1@hotmail.com>
2016-05-11 11:27:08 +0200

    More failing tests.

---
+++ Gu.State.Tests/Track/Dirty/DirtyTrackerTests.Verify.cs
@@ -0,0 +1,22 @@
+﻿namespace Gu.State.Tests
+{
+    using NUnit.Framework;
+
+    using static DirtyTrackerTypes;
+
+    public partial class DirtyTrackerTests
+    {
+        public class Verify
+        {
+            [TestCase(ReferenceHandling.Throw)]
+            [TestCase(ReferenceHandling.References)]
+            [TestCase(ReferenceHandling.Structural)]
+            public void WithSimpleProperties(ReferenceHandling referenceHandling)
+            {
+                Track.VerifyCanTrackIsDirty<WithSimpleProperties>(referenceHandling);
+                Track.VerifyCanTrackIsDirty<WithSimpleProperties>(PropertiesSettings.GetOrCreate(referenceHandling));
+                Track.VerifyCanTrackIsDirty(typeof(WithSimpleProperties), PropertiesSettings.GetOrCreate(referenceHandling));
+            }
+        }
+    }
+}
