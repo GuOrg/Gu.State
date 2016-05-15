b3668d04bca445557497d661e82e8f5a16cf7c60
Johan Larsson <johan_gavle1@hotmail.com>
2016-05-15 11:15:12 +0200

    Failing test, reminder.

---
+++ Gu.State.Tests/Track/Dirty/DirtyTrackerTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Gu.State.Tests
+{
+    using NUnit.Framework;
+
+    public partial class DirtyTrackerTests
+    {
+        public class Levels
+        {
+            [Test]
+            public void SecondLavelChanges()
+            {
+                Assert.Fail("Write this test");
+            }
+        }
+    }
+}
