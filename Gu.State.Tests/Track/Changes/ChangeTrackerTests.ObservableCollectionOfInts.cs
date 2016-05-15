39010f5ebe36b84797ea08744b20c61bd7753ae5
Johan Larsson <johan_gavle1@hotmail.com>
2016-05-15 15:54:12 +0200

    Fix more tests.

--- Gu.State.Tests/Track/Changes/ChangeTrackerTests.ObservableCollectionOfInts.cs
+++ Gu.State.Tests/Track/Changes/ChangeTrackerTests.ObservableCollectionOfInts.cs
@@ -61,22 +61,33 @@ namespace Gu.State.Tests
             [TestCase(ReferenceHandling.References)]
             public void Add(ReferenceHandling referenceHandling)
             {
-                var changes = new List<object>();
                 var source = new ObservableCollection<int>();
+                var propertyChanges = new List<string>();
+                var changes = new List<EventArgs>();
                 using (var tracker = Track.Changes(source, referenceHandling))
                 {
-                    tracker.PropertyChanged += (_, e) => changes.Add(e.PropertyName);
+                    tracker.PropertyChanged += (_, e) => propertyChanges.Add(e.PropertyName);
                     tracker.Changed += (_, e) => changes.Add(e);
                     Assert.AreEqual(0, tracker.Changes);
+                    CollectionAssert.IsEmpty(propertyChanges);
                     CollectionAssert.IsEmpty(changes);
 
                     source.Add(1);
                     Assert.AreEqual(1, tracker.Changes);
-                    CollectionAssert.AreEqual(CreateExpectedChangeArgs(1), changes);
+                    CollectionAssert.AreEqual(new[] { "Changes" }, propertyChanges);
+                    var node = ChangeTrackerNode.GetOrCreate((INotifyCollectionChanged)source, tracker.Settings, false).Value;
+                    var expected = new[] { RootChangeEventArgs.Create(node, new AddEventArgs(0)) };
+                    CollectionAssert.AreEqual(expected, changes, EventArgsComparer.Default);
 
                     source.Add(2);
                     Assert.AreEqual(2, tracker.Changes);
-                    CollectionAssert.AreEqual(CreateExpectedChangeArgs(2), changes);
+                    CollectionAssert.AreEqual(new[] { "Changes", "Changes" }, propertyChanges);
+                    expected = new[]
+                                   {
+                                       RootChangeEventArgs.Create(node, new AddEventArgs(0)),
+                                       RootChangeEventArgs.Create(node, new AddEventArgs(1))
+                                   };
+                    CollectionAssert.AreEqual(expected, changes, EventArgsComparer.Default);
                 }
             }
 
@@ -85,22 +96,33 @@ namespace Gu.State.Tests
             [TestCase(ReferenceHandling.References)]
             public void Remove(ReferenceHandling referenceHandling)
             {
-                var changes = new List<object>();
                 var source = new ObservableCollection<int> { 1, 2 };
+                var propertyChanges = new List<string>();
+                var changes = new List<EventArgs>();
                 using (var tracker = Track.Changes(source, referenceHandling))
                 {
-                    tracker.PropertyChanged += (_, e) => changes.Add(e.PropertyName);
+                    tracker.PropertyChanged += (_, e) => propertyChanges.Add(e.PropertyName);
                     tracker.Changed += (_, e) => changes.Add(e);
                     Assert.AreEqual(0, tracker.Changes);
+                    CollectionAssert.IsEmpty(propertyChanges);
                     CollectionAssert.IsEmpty(changes);
 
                     source.RemoveAt(1);
                     Assert.AreEqual(1, tracker.Changes);
-                    CollectionAssert.AreEqual(CreateExpectedChangeArgs(1), changes);
+                    CollectionAssert.AreEqual(new[] { "Changes" }, propertyChanges);
+                    var node = ChangeTrackerNode.GetOrCreate((INotifyCollectionChanged)source, tracker.Settings, false).Value;
+                    var expected = new[] { RootChangeEventArgs.Create(node, new RemoveEventArgs(1)) };
+                    CollectionAssert.AreEqual(expected, changes, EventArgsComparer.Default);
 
                     source.RemoveAt(0);
                     Assert.AreEqual(2, tracker.Changes);
-                    CollectionAssert.AreEqual(CreateExpectedChangeArgs(2), changes);
+                    CollectionAssert.AreEqual(new[] { "Changes" }, propertyChanges);
+                    expected = new[]
+                                   {
+                                       RootChangeEventArgs.Create(node, new RemoveEventArgs(1)),
+                                       RootChangeEventArgs.Create(node, new RemoveEventArgs(0))
+                                   };
+                    CollectionAssert.AreEqual(expected, changes, EventArgsComparer.Default);
                 }
             }
 
@@ -109,22 +131,33 @@ namespace Gu.State.Tests
             [TestCase(ReferenceHandling.References)]
             public void Clear(ReferenceHandling referenceHandling)
             {
-                var changes = new List<object>();
                 var source = new ObservableCollection<int> { 1, 2 };
+                var propertyChanges = new List<string>();
+                var changes = new List<EventArgs>();
                 using (var tracker = Track.Changes(source, referenceHandling))
                 {
-                    tracker.PropertyChanged += (_, e) => changes.Add(e.PropertyName);
+                    tracker.PropertyChanged += (_, e) => propertyChanges.Add(e.PropertyName);
                     tracker.Changed += (_, e) => changes.Add(e);
                     Assert.AreEqual(0, tracker.Changes);
+                    CollectionAssert.IsEmpty(propertyChanges);
                     CollectionAssert.IsEmpty(changes);
 
                     source.Clear();
                     Assert.AreEqual(1, tracker.Changes);
-                    CollectionAssert.AreEqual(CreateExpectedChangeArgs(1), changes);
+                    CollectionAssert.AreEqual(new[] { "Changes" }, propertyChanges);
+                    var node = ChangeTrackerNode.GetOrCreate((INotifyCollectionChanged)source, tracker.Settings, false).Value;
+                    var expected = new[] { RootChangeEventArgs.Create(node, new ResetEventArgs()) };
+                    CollectionAssert.AreEqual(expected, changes, EventArgsComparer.Default);
 
                     source.Clear();
                     Assert.AreEqual(2, tracker.Changes);
-                    CollectionAssert.AreEqual(CreateExpectedChangeArgs(2), changes);
+                    CollectionAssert.AreEqual(new[] { "Changes", "Changes" }, propertyChanges);
+                    expected = new[]
+                                   {
+                                       RootChangeEventArgs.Create(node, new ResetEventArgs()),
+                                       RootChangeEventArgs.Create(node, new ResetEventArgs())
+                                   };
+                    CollectionAssert.AreEqual(expected, changes, EventArgsComparer.Default);
                 }
             }
 
@@ -133,18 +166,23 @@ namespace Gu.State.Tests
             [TestCase(ReferenceHandling.References)]
             public void Replace(ReferenceHandling referenceHandling)
             {
-                var changes = new List<object>();
                 var source = new ObservableCollection<int> { 1, 2 };
+                var propertyChanges = new List<string>();
+                var changes = new List<EventArgs>();
                 using (var tracker = Track.Changes(source, referenceHandling))
                 {
-                    tracker.PropertyChanged += (_, e) => changes.Add(e.PropertyName);
+                    tracker.PropertyChanged += (_, e) => propertyChanges.Add(e.PropertyName);
                     tracker.Changed += (_, e) => changes.Add(e);
                     Assert.AreEqual(0, tracker.Changes);
+                    CollectionAssert.IsEmpty(propertyChanges);
                     CollectionAssert.IsEmpty(changes);
 
                     source[0] = 3;
                     Assert.AreEqual(1, tracker.Changes);
-                    CollectionAssert.AreEqual(CreateExpectedChangeArgs(1), changes);
+                    CollectionAssert.AreEqual(new[] { "Changes" }, propertyChanges);
+                    var node = ChangeTrackerNode.GetOrCreate((INotifyCollectionChanged)source, tracker.Settings, false).Value;
+                    var expected = new[] { RootChangeEventArgs.Create(node, new ReplaceEventArgs(0)) };
+                    CollectionAssert.AreEqual(expected, changes, EventArgsComparer.Default);
                 }
             }
 
