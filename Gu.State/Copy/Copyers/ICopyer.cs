35b1432258acbf4792d3426e26f714dd2657a358
Johan Larsson <johan_gavle1@hotmail.com>
2016-05-27 16:57:04 +0200

    More docs.

--- Gu.State/Copy/Copyers/ICopyer.cs
+++ Gu.State/Copy/Copyers/ICopyer.cs
@@ -1,7 +1,14 @@
 ﻿namespace Gu.State
 {
+    /// <summary>A custom copy implementation.</summary>
     public interface ICopyer
     {
+        /// <summary>Copy <paramref name="source"/> to <paramref name="target"/> or create a new value if target is null.</summary>
+        /// <typeparam name="TSettings">The setting type.</typeparam>
+        /// <param name="source">The source value.</param>
+        /// <param name="target">The target value.</param>
+        /// <param name="settings">The settings.</param>
+        /// <param name="referencePairs">A collection with previously copied values.</param>
         void Copy<TSettings>(
             object source,
             object target,
