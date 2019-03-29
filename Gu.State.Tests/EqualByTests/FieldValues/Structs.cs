41d9abb6d997e6bf39f8557669070b9bfbeea790
JohanLarsson <johan_gavle1@hotmail.com>
2019-03-29 09:31:06 +0100

    Test EqualBy with structs.

---
+++ Gu.State.Tests/EqualByTests/FieldValues/Structs.cs
@@ -0,0 +1,30 @@
+﻿namespace Gu.State.Tests.EqualByTests.FieldValues
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Structs : StructsTests
+    {
+        public override bool EqualMethod<T>(
+            T x,
+            T y,
+            ReferenceHandling referenceHandling = ReferenceHandling.Structural,
+            string excludedMembers = null,
+            Type ignoredType = null)
+        {
+            var builder = FieldsSettings.Build();
+            if (excludedMembers != null)
+            {
+                builder.AddIgnoredField<T>(excludedMembers);
+            }
+
+            if (ignoredType != null)
+            {
+                builder.IgnoreType(ignoredType);
+            }
+
+            var settings = builder.CreateSettings(referenceHandling);
+            return EqualBy.FieldValues(x, y, settings);
+        }
+    }
+}
---
+++ Gu.State.Tests/EqualByTests/PropertyValues/Structs.cs
@@ -0,0 +1,24 @@
+﻿namespace Gu.State.Tests.EqualByTests.PropertyValues
+{
+    using System;
+
+    public class Structs : StructsTests
+    {
+        public override bool EqualMethod<T>(T x, T y, ReferenceHandling referenceHandling = ReferenceHandling.Structural, string excludedMembers = null, Type ignoredType = null)
+        {
+            var builder = PropertiesSettings.Build();
+            if (excludedMembers != null)
+            {
+                builder.IgnoreProperty<T>(excludedMembers);
+            }
+
+            if (ignoredType != null)
+            {
+                builder.IgnoreType(ignoredType);
+            }
+
+            var settings = builder.CreateSettings(referenceHandling);
+            return EqualBy.PropertyValues(x, y, settings);
+        }
+    }
+}
