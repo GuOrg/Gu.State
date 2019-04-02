af3ab866f9e42093f6a1efbfdeb09d3af6c96609
JohanLarsson <johan_gavle1@hotmail.com>
2019-04-02 12:41:53 +0200

    Cleanup SetEqualByComparer

--- Gu.State/EqualBy/Comparers/SetEqualByComparer.cs
+++ Gu.State/EqualBy/Comparers/SetEqualByComparer.cs
@@ -3,7 +3,6 @@ namespace Gu.State
     using System;
     using System.Collections.Generic;
     using System.Reflection;
-    using System.Runtime.CompilerServices;
 
     internal static class SetEqualByComparer
     {
@@ -34,51 +33,35 @@ namespace Gu.State
             }
 
             /// <inheritdoc />
-            public override bool Equals(
-                object x,
-                object y,
-                MemberSettings settings,
-                ReferencePairCollection referencePairs)
+            public override bool Equals(object x, object y, MemberSettings settings, ReferencePairCollection referencePairs)
             {
                 if (TryGetEitherNullEquals(x, y, out var result))
                 {
                     return result;
                 }
 
-                var xs = (ISet<T>)x;
-                var ys = (ISet<T>)y;
-                if (xs.Count != ys.Count)
+                return Equals((IReadOnlyCollection<T>)x, (IReadOnlyCollection<T>)y, settings, referencePairs);
+            }
+
+            private static bool Equals(IReadOnlyCollection<T> x, IReadOnlyCollection<T> y, MemberSettings settings, ReferencePairCollection referencePairs)
+            {
+                if (x.Count != y.Count)
                 {
                     return false;
                 }
 
-                var isEquatable = settings.IsEquatable(x.GetType().GetItemType());
-                var xHashSet = xs as HashSet<T>;
-                if (isEquatable)
+                if (x is HashSet<T> xs &&
+                    typeof(T).IsSealed &&
+                    settings.IsEquatable(x.GetType().GetItemType()))
                 {
-                    if (Equals(xHashSet?.Comparer, EqualityComparer<T>.Default))
-                    {
-                        return xs.SetEquals(ys);
-                    }
-
-                    return this.ItemsEquals(xs, ys, EqualityComparer<T>.Default.Equals, EqualityComparer<T>.Default.GetHashCode);
+                    return xs.SetEquals(y);
                 }
 
-                if (settings.ReferenceHandling == ReferenceHandling.References)
-                {
-                    return this.ItemsEquals(xs, ys, (xi, yi) => ReferenceEquals(xi, yi), xi => RuntimeHelpers.GetHashCode(xi));
-                }
-
-                var hashCodeMethod = typeof(T).GetMethod(nameof(this.GetHashCode), BindingFlags.Public | BindingFlags.Instance, null, Type.EmptyTypes, null);
-                if (hashCodeMethod.DeclaringType == typeof(object))
-                {
-                    return this.ItemsEquals(xs, ys, (xi, yi) => EqualBy.MemberValues(xi, yi, settings, referencePairs), _ => 0);
-                }
-
-                return this.ItemsEquals(xs, ys, (xi, yi) => EqualBy.MemberValues(xi, yi, settings, referencePairs), xi => xi.GetHashCode());
+                var comparer = settings.GetEqualByComparer(typeof(T), checkReferenceHandling: true);
+                return ItemsEquals(x, y, (xi, yi) => comparer.Equals(xi, yi, settings, referencePairs), xi => xi.GetHashCode());
             }
 
-            private bool ItemsEquals(ISet<T> x, ISet<T> y, Func<T, T, bool> compare, Func<T, int> getHashCode)
+            private static bool ItemsEquals(IReadOnlyCollection<T> x, IReadOnlyCollection<T> y, Func<T, T, bool> compare, Func<T, int> getHashCode)
             {
                 using (var borrow = HashSetPool<T>.Borrow(compare, getHashCode))
                 {
