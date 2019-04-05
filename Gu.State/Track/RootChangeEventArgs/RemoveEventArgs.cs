ab944be684822f0e7d406acd6e54ef5598530e2d
JohanLarsson <johan_gavle1@hotmail.com>
2019-04-05 10:44:52 +0200

    RemoveEventArgs : IEquatable<RemoveEventArgs>

--- Gu.State/Track/RootChangeEventArgs/RemoveEventArgs.cs
+++ Gu.State/Track/RootChangeEventArgs/RemoveEventArgs.cs
@@ -1,9 +1,10 @@
 namespace Gu.State
 {
+    using System;
     using System.Collections;
 
     /// <summary>This is raised when an element is removed from a notifying collection.</summary>
-    public struct RemoveEventArgs : IRootChangeEventArgs
+    public struct RemoveEventArgs : IRootChangeEventArgs, IEquatable<RemoveEventArgs>
     {
         internal RemoveEventArgs(IList source, int index)
         {
@@ -19,5 +20,32 @@ namespace Gu.State
 
         /// <summary>Gets the index at which an element was removed. </summary>
         public int Index { get; }
+
+        public static bool operator ==(RemoveEventArgs left, RemoveEventArgs right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(RemoveEventArgs left, RemoveEventArgs right)
+        {
+            return !left.Equals(right);
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj) => obj is RemoveEventArgs other &&
+                                                   this.Equals(other);
+
+        /// <inheritdoc />
+        public bool Equals(RemoveEventArgs other) => Equals(this.Source, other.Source) &&
+                                                     this.Index == other.Index;
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((this.Source != null ? this.Source.GetHashCode() : 0) * 397) ^ this.Index;
+            }
+        }
     }
 }
