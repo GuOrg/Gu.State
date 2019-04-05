ffd03c10448a358b4c4e518b7aa11f27bcfc11cf
JohanLarsson <johan_gavle1@hotmail.com>
2019-04-05 12:08:13 +0200

    AddEventArgs : IEquatable<AddEventArgs>

--- Gu.State/Track/RootChangeEventArgs/AddEventArgs.cs
+++ Gu.State/Track/RootChangeEventArgs/AddEventArgs.cs
@@ -1,9 +1,10 @@
 namespace Gu.State
 {
+    using System;
     using System.Collections;
 
     /// <summary>This is raised when an element was added to a notifying collection.</summary>
-    public struct AddEventArgs : IRootChangeEventArgs
+    public struct AddEventArgs : IRootChangeEventArgs, IEquatable<AddEventArgs>
     {
         internal AddEventArgs(IList source, int index)
         {
@@ -19,5 +20,32 @@ namespace Gu.State
 
         /// <summary>Gets the index at which an element was added. </summary>
         public int Index { get; }
+
+        public static bool operator ==(AddEventArgs left, AddEventArgs right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(AddEventArgs left, AddEventArgs right)
+        {
+            return !left.Equals(right);
+        }
+
+        /// <inheritdoc />
+        public bool Equals(AddEventArgs other) => Equals(this.Source, other.Source) &&
+                                                  this.Index == other.Index;
+
+        /// <inheritdoc />
+        public override bool Equals(object obj) => obj is AddEventArgs other &&
+                                                   this.Equals(other);
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((this.Source != null ? this.Source.GetHashCode() : 0) * 397) ^ this.Index;
+            }
+        }
     }
 }
