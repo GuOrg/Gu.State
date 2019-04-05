dc57570e9247a7c4d9799e1687a4b822de58de35
JohanLarsson <johan_gavle1@hotmail.com>
2019-04-05 14:56:39 +0200

    Override equality for structs.

--- Gu.State/Track/RootChangeEventArgs/PropertyChangeEventArgs.cs
+++ Gu.State/Track/RootChangeEventArgs/PropertyChangeEventArgs.cs
@@ -1,8 +1,9 @@
 namespace Gu.State
 {
+    using System;
     using System.Reflection;
 
-    public struct PropertyChangeEventArgs : IRootChangeEventArgs
+    public struct PropertyChangeEventArgs : IRootChangeEventArgs, IEquatable<PropertyChangeEventArgs>
     {
         public PropertyChangeEventArgs(object source, PropertyInfo propertyInfo)
         {
@@ -14,5 +15,32 @@ namespace Gu.State
         public object Source { get; }
 
         public PropertyInfo PropertyInfo { get; }
+
+        public static bool operator ==(PropertyChangeEventArgs left, PropertyChangeEventArgs right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(PropertyChangeEventArgs left, PropertyChangeEventArgs right)
+        {
+            return !left.Equals(right);
+        }
+
+        /// <inheritdoc />
+        public bool Equals(PropertyChangeEventArgs other) => Equals(this.Source, other.Source) &&
+                                                             Equals(this.PropertyInfo, other.PropertyInfo);
+
+        /// <inheritdoc />
+        public override bool Equals(object obj) => obj is PropertyChangeEventArgs other &&
+                                                   this.Equals(other);
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((this.Source != null ? this.Source.GetHashCode() : 0) * 397) ^ (this.PropertyInfo != null ? this.PropertyInfo.GetHashCode() : 0);
+            }
+        }
     }
 }
--- Gu.State/Track/RootChangeEventArgs/ReplaceEventArgs.cs
+++ Gu.State/Track/RootChangeEventArgs/ReplaceEventArgs.cs
@@ -1,9 +1,10 @@
 namespace Gu.State
 {
+    using System;
     using System.Collections;
 
     /// <summary>This is raised when an element was replaced in a notifying collection.</summary>
-    public struct ReplaceEventArgs : IRootChangeEventArgs
+    public struct ReplaceEventArgs : IRootChangeEventArgs, IEquatable<ReplaceEventArgs>
     {
         internal ReplaceEventArgs(IList source, int index)
         {
@@ -17,7 +18,34 @@ namespace Gu.State
         /// <inheritdoc />
         object IRootChangeEventArgs.Source => this.Source;
 
+        public static bool operator ==(ReplaceEventArgs left, ReplaceEventArgs right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(ReplaceEventArgs left, ReplaceEventArgs right)
+        {
+            return !left.Equals(right);
+        }
+
         /// <summary>Gets the index at which an element was replaced. </summary>
         public int Index { get; }
+
+        /// <inheritdoc />
+        public bool Equals(ReplaceEventArgs other) => Equals(this.Source, other.Source) &&
+                                                      this.Index == other.Index;
+
+        /// <inheritdoc />
+        public override bool Equals(object obj) => obj is ReplaceEventArgs other &&
+                                                   this.Equals(other);
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((this.Source != null ? this.Source.GetHashCode() : 0) * 397) ^ this.Index;
+            }
+        }
     }
 }
